b282d274c9e10cf174e66ff56b3c3cdd1f8feb0d
aryanisha1020-commits <aryanisha1020@gmail.com>
2025-09-21 10:02:23 +0530

    task 1 completed

---
+++ Task 1.py
@@ -0,0 +1,10 @@
+#Task 1: Check if a Number is Even or Odd
+
+Num = int(input("Enter a number: "))
+
+if Num%2 == 0 :
+    print(Num,"is an even number")
+else:
+    print(Num,"is an odd number")
+
+print("Thank you")
